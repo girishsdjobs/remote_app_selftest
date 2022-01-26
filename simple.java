097f442c052200b2701fa9600fb3149821c4c42e
edureka-transaction-api <girishsdjobs@gmail.com>
2022-01-26 19:16:12 +0530

    enhanced the code commit of simple.java file

--- simple.java
+++ simple.java
@@ -5,3 +5,13 @@ class Simple{
 
 	}
 }
+
+class FirstProgram
+{ public static void main (string[]args)
+	{
+		int a = 20, b = 30, c;
+		c = a + b;
+		System.out.println ("Result is :" + c);
+	}
+}
+
